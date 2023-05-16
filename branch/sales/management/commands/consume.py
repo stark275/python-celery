05d176928fb026ca6e4b2f19eb038fcc2f4182fa
stark <stark1_5@live.com>
2023-05-16 15:17:29 +0200

    Branch: Consume and save in DB

---
+++ branch/sales/management/commands/consume.py
@@ -0,0 +1,35 @@
+import pika
+from django.utils import timezone
+
+from django.core.management.base import BaseCommand, CommandError
+from ...models  import Message, Sale
+
+class Command(BaseCommand):
+   
+    def handle(self, *args, **options):
+
+        credentials = pika.PlainCredentials('l1fasi', '123456789')
+        connexion = pika.BlockingConnection(
+            pika.ConnectionParameters(
+                host='localhost',
+                port=5672,
+                virtual_host='parallel_programming',
+                credentials=credentials
+            )
+        )
+
+        channel = connexion.channel()
+
+        channel.basic_consume(queue='matadi', on_message_callback=self.get_messages, auto_ack=True)
+        self.stdout.write(
+                self.style.SUCCESS("Started Consuming...")
+            )
+        channel.start_consuming()
+        
+    def get_messages(ch, method, properties, body, b):
+        message = Message(content=str(b), pub_date=timezone.now() )
+        message.save()
+        
+        print("[-] Receved: %r" % b)
+
+        
